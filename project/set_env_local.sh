7f52aceca116cdea3db3b829ef38e7deb0ef0494
ductruong253 <ddtruong94@gmail.com>
2022-07-30 13:51:18 +0700

    update progress

---
+++ project/set_env_local.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=admin
+export POSTGRES_HOST=127.0.0.1
+export POSTGRES_DB=udagram
+export AWS_BUCKET=arn:aws:s3:::udaconnect
+export AWS_REGION=us-west-2
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
